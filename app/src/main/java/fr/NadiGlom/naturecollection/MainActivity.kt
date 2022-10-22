5e3af298cd6129c99ef0ec0a26df627ad29838df
NadiGlom <glomotnad@gmail.com>
2022-10-22 10:59:47 +0200

    57 minutes graven

--- app/src/main/java/fr/NadiGlom/naturecollection/MainActivity.kt
+++ app/src/main/java/fr/NadiGlom/naturecollection/MainActivity.kt
@@ -2,10 +2,17 @@ package fr.NadiGlom.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.NadiGlom.naturecollection.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //injecter le fragment dans notre boîte (fragment_container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
